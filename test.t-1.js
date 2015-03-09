63e779b02d03a8c639744afa80c7c11259409d89
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-03-08 22:21:57 -0400

    t-1

--- test.t-1.js
+++ test.t-1.js
@@ -1,9 +1,16 @@
 ///<reference path='Scripts/typings/mocha/mocha.d.ts'/>
 ///<reference path='Scripts/typings/chai/chai.d.ts'/>
 describe('t_1.StringMatch', function () {
-    var sm = new t_1.StringMatch(['doesNotExist'], 'I am here');
-    it('should find no matches', function () {
-        chai.expect(sm.posOfHead).to.equal(-1);
+    var test1 = new t_1.StringMatch(['doesNotExist'], 'I am here');
+    it('new t_1.StringMatch(["doesNotExist"], "I am here") should find no matches', function () {
+        chai.expect(test1.posOfHead).to.equal(-1);
+    });
+    var test2 = new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships?');
+    debugger;
+    it("new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships? ') should find a match", function () {
+        chai.expect(test2.posOfHead).to.equal(3);
     });
 });
+//describe('t_1.StringMatch2',() => {
+//}); 
 //# sourceMappingURL=test.t-1.js.map
--- test.t-1.ts
+++ test.t-1.ts
@@ -1,8 +1,16 @@
 ﻿///<reference path='Scripts/typings/mocha/mocha.d.ts'/>
 ///<reference path='Scripts/typings/chai/chai.d.ts'/>
 describe('t_1.StringMatch',() => {
-    var sm = new t_1.StringMatch(['doesNotExist'], 'I am here');
-    it('should find no matches', () => {
-        chai.expect(sm.posOfHead).to.equal(-1);
+    var test1 = new t_1.StringMatch(['doesNotExist'], 'I am here');
+    it('new t_1.StringMatch(["doesNotExist"], "I am here") should find no matches',() => {
+        chai.expect(test1.posOfHead).to.equal(-1);
+    });
+    var test2 = new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships?');
+    debugger;
+    it("new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships? ') should find a match",() => {
+        chai.expect(test2.posOfHead).to.equal(3);
     });
 });
+//describe('t_1.StringMatch2',() => {
+    
+//});
