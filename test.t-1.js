c0a3594ddc37cb6648de7fd93e5d6d3b8cc922d4
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-03-08 22:26:35 -0400

    t-1

--- test.t-1.js
+++ test.t-1.js
@@ -1,13 +1,12 @@
 ///<reference path='Scripts/typings/mocha/mocha.d.ts'/>
 ///<reference path='Scripts/typings/chai/chai.d.ts'/>
 describe('t_1.StringMatch', function () {
-    var test1 = new t_1.StringMatch(['doesNotExist'], 'I am here');
-    it('new t_1.StringMatch(["doesNotExist"], "I am here") should find no matches', function () {
+    it('Test 1.  No matches', function () {
+        var test1 = new t_1.StringMatch(['doesNotExist'], 'I am here');
         chai.expect(test1.posOfHead).to.equal(-1);
     });
-    var test2 = new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships?');
-    debugger;
-    it("new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships? ') should find a match", function () {
+    it("Test 2.  A match", function () {
+        var test2 = new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships?');
         chai.expect(test2.posOfHead).to.equal(3);
     });
 });
--- test.t-1.ts
+++ test.t-1.ts
@@ -1,13 +1,12 @@
 ﻿///<reference path='Scripts/typings/mocha/mocha.d.ts'/>
 ///<reference path='Scripts/typings/chai/chai.d.ts'/>
 describe('t_1.StringMatch',() => {
-    var test1 = new t_1.StringMatch(['doesNotExist'], 'I am here');
-    it('new t_1.StringMatch(["doesNotExist"], "I am here") should find no matches',() => {
+    it('Test 1.  No matches',() => {
+        var test1 = new t_1.StringMatch(['doesNotExist'], 'I am here');
         chai.expect(test1.posOfHead).to.equal(-1);
     });
-    var test2 = new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships?');
-    debugger;
-    it("new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships? ') should find a match",() => {
+    it("Test 2.  A match",() => {
+        var test2 = new t_1.StringMatch(['th', 'th'], 'Is this the face that launched a thousand ships?');
         chai.expect(test2.posOfHead).to.equal(3);
     });
 });
